0dd6b066b003c8b05479fea8ffbe81795084f213
manuelatrotta <manuelat88@hotmail.it>
2020-02-25 18:52:15 +0100

    titoli e p

--- index.php
+++ index.php
@@ -13,24 +13,25 @@ include __DIR__ . '/partials/server.php';
      <title>php-ajax-dischi</title>
    </head>
    <body>
+    <!--header-->
      <header>
        <div class="container">
         <img class="logo" src="dist/img/logo.png" alt="logo spotify">
      </header>
+     <!--/header-->
+     <!--main-->
      <main>
        <section class="selection_album">
         <?php foreach ($database as $album) { ?>
          <div class="album">
            <img class="poster" src="<?php echo $album["poster"]; ?>" alt="">
-           <h2><?php echo $album["title"]; ?></h2>
+           <h3 class="title"><?php echo $album["title"]; ?></h3>
            <h4><?php echo $album["author"]; ?></h4>
            <p><?php echo $album["year"]; ?></p>
          </div>
         <?php  } ?>
-
        </section>
-
      </main>
-
+    <!--/main-->
    </body>
  </html>
