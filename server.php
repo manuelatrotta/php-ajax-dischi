dd3ad086c665dd7606698c986cb69ae741ce1163
manuelatrotta <manuelat88@hotmail.it>
2020-02-25 16:13:08 +0100

    inserimento 10 album

--- server.php
+++ server.php
@@ -7,10 +7,62 @@ $database = [
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'KarmaPolice',
+      'author' => 'radiohead',
+      'year' => 1997,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71XzAV2UQQL._SX466_.jpg'
     ],
+    [
+      'title' => 'Waiting for the sun',
+      'author' => 'The Doors',
+      'year' => 1968,
+      'poster' => 'https://m.media-amazon.com/images/I/91MGIfPenWL._SS500_.jpg'
+    ],
+    [
+      'title' => 'Remasted',
+      'author' => 'Led Zeppelin',
+      'year' => 1990,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51FBvmoThXL._SY355_.jpg'
+    ],
+    [
+      'title' => 'Without You',
+      'author' => 'Placebo',
+      'year' => 1998 ,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71yrdcdufmL._SL1104_.jpg'
+    ],
+    [
+      'title' => 'Violator',
+      'author' => 'Depeche Mode',
+      'year' => 1990,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/714McwlTjNL._SX466_.jpg'
+    ],
+    [
+      'title' => 'Deliverance',
+      'author' => 'Opeth',
+      'year' => 1999,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51B2%2BVgb%2BpL._SX425_.jpg'
+    ],
+    [
+      'title' => 'The black album',
+      'author' => 'Metallica',
+      'year' => 1990,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51O7k2hqS1L._SL1417_.jpg'
+    ],
+    [
+      'title' => '()',
+      'author' => 'Sigur Ros',
+      'year' => 2018,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/615Pf7ux8pL._SX466_.jpg'
+    ],
+    [
+      'title' => 'The number of the Beast',
+      'author' => 'Iron Maiden',
+      'year' => 1982,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/61XFRtHPbaL.jpg'
+    ],
+
+
+
+
 ]
  ?>
