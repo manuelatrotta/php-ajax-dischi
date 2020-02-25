f5a26c3caed0ad45413583bf928436ea9fa4513e
manuelatrotta <manuelat88@hotmail.it>
2020-02-25 16:24:06 +0100

    inizio struttura html e color palette

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
-include __DIR__ . '/server.php';
+include __DIR__ . '/partials/server.php';
 
 var_dump($database);
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <header>
+       <img src="" alt="spotify-logo">
+
+     </header>
+     <main>
+       <section class="selection_album">
+         <div class="">
+           <img src="" alt="">
+           <h2></h2>
+           <h4></h4>
+           <p></p>
+         </div>
+
+       </section>
+
+     </main>
+
+   </body>
+ </html>
