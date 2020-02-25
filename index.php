f9bff6bfc882898aefa9227b5a30dca2f4ec8a0f
manuelatrotta <manuelat88@hotmail.it>
2020-02-25 17:25:20 +0100

    impostazione album

--- index.php
+++ index.php
@@ -9,16 +9,19 @@ include __DIR__ . '/partials/server.php';
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
-     <title></title>
+     <link rel="stylesheet" href="dist/app.css">
+     <title>php-ajax-dischi</title>
    </head>
    <body>
      <header>
+       <div class="container">
+        <img class="logo" src="dist/img/logo.png" alt="logo spotify">
      </header>
      <main>
        <section class="selection_album">
         <?php foreach ($database as $album) { ?>
-         <div class="">
-           <img src="<?php echo $album["poster"]; ?>" alt="">
+         <div class="album">
+           <img class="poster" src="<?php echo $album["poster"]; ?>" alt="">
            <h2><?php echo $album["title"]; ?></h2>
            <h4><?php echo $album["author"]; ?></h4>
            <p><?php echo $album["year"]; ?></p>
